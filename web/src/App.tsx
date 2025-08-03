50aca3381c5fb183bfae54fad980e3186779a03a
t9tieanh <phama9162@gmail.com>
2025-08-03 14:06:26 +0700

    init project

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { FC, useState } from 'react'
+import reactlogo from './assets/images/react.svg'
+import './assets/styles/app.css'
+import './assets/styles/app.scss'
+
+const App: FC = () => {
+  const [fullname, setFullname] = useState('Phạm Tiến Anh')
+  return (
+    <div>
+      <img src={reactlogo} alt='React Logo' width={100} height={100} />
+      <h1>{fullname}</h1>
+      <h2>Bắt đầu thôi, web bạn đang chạy trên ... {process.env.HOST}</h2>
+    </div>
+  )
+}
+
+export default App
