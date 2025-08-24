ef05842ca8b8f2d3bdaee8550f30749bc793f101
t9tieanh <phama9162@gmail.com>
2025-08-24 19:54:45 +0700

    init sale service

---
+++ rest-api/sale-service/src/test/java/com/freeclassroom/saleservice/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeclassroom.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
