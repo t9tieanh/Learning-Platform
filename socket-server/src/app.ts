1c9251b057f69f75bda9b646b23a2b248be446dc
t9tieanh <phama9162@gmail.com>
2026-01-14 17:08:18 +0700

    chore:add after script rm rf sensitive file

--- socket-server/src/app.ts
+++ socket-server/src/app.ts
@@ -7,22 +7,6 @@ export const initSocket = async (httpServer: http.Server) => {
         cors: { origin: "*" }
     });
 
-    // Check authen before connect socket
-    // io.use((socket, next) => {
-    //     const token = socket.handshake.auth?.token; 
-    //     if (!token) {
-    //         return next(new Error("No token provided"));
-    //     }
-
-    //     try {
-    //         const user = verifyToken(token);  // grpc user-service
-    //         (socket as any).user = user; 
-    //         next();
-    //     } catch (err) {
-    //         next(new Error("Invalid token"));
-    //     }
-    // });
-
 
     io.on("connection", (socket) => {
         console.log(`Client connected: ${socket.id}`);
